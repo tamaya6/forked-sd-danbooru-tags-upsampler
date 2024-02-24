649e79f76c04967915851564db71fe24140157b7
tamaya6 <tamaya6@gmail.com>
2024-02-25 01:01:08 +0900

    修正: 参照ファイル修正

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ import sys, os, shutil
 import folder_paths
 
 sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
-from .example_node import DanbooruTagUpsampler
+from .node import DanbooruTagUpsampler
 module_root_directory = os.path.dirname(os.path.realpath(__file__))
 
 NODE_CLASS_MAPPINGS = {
-    "Danbooru Tag Upsampler": DanbooruTagUpsampler,
+    "DanbooruTagUpsampler": DanbooruTagUpsampler,
                       }
 
 __all__ = ["NODE_CLASS_MAPPINGS"]
